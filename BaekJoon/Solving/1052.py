147236e028cab9c34eab4a0df2fe646ac4bf1829
seongjxnkit <j267175@kumoh.ac.kr>
2023-01-16 00:54:18 +0900

    23.01.16. solving

---
+++ BaekJoon/Solving/1052.py
@@ -0,0 +1,4 @@
+# BOJ 1052 : 물병
+# https://www.acmicpc.net/problem/1052
+
+N, K = map(int, input().split())
---
+++ BaekJoon/Solving/1541.py
@@ -0,0 +1,9 @@
+# BOJ 1541 : 잃어버린 괄호
+# https://www.acmicpc.net/problem/1541
+
+import re
+
+n = input()
+
+m = re.split('[0-9]*', n)
+print(m)
