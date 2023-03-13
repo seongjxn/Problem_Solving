9a6981d8f9fdfffe5373319f843afc57b05198a2
seongjxn <seongjxn@ivyforest.kr>
2023-03-13 16:39:18 +0900

    23.03.13. Rename

---
+++ BaekJoon/Solved/1000.py
@@ -0,0 +1,6 @@
+# BOJ 1000 : A+B
+# https://www.acmicpc.net/problem/1000
+
+
+a, b = map(int, input().split())
+print(a+b)
---
+++ BaekJoon/Solved/1001.py
@@ -0,0 +1,6 @@
+# BOJ 1001 : 
+# https://www.acmicpc.net/problem/1001
+
+
+a, b = map(int, input().split())
+print(a - b)
