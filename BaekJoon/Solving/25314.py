d9560bd4db99625907eddf1f58a5491401d93cd3
seongjxn <seongjxn@ivyforest.kr>
2023-09-07 21:44:49 +0900

    25314 - 코딩은 체육과목 입니다

---
+++ BaekJoon/Solving/25314.py
@@ -0,0 +1,10 @@
+# BOJ 25314 : 코딩은 체육과목 입니다
+# https://www.acmicpc.net/problem/25314
+
+
+import sys
+N = int(sys.stdin.readline().rstrip())
+
+for i in range(0, N, 4):
+    print('long', end=' ')
+print('int')
