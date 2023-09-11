dd1b9a4cb357b5fea6cf7faca2f01167a4826bc8
seongjxn <seongjxn@ivyforest.kr>
2023-09-11 22:56:18 +0900

    5338 - 마이크로소프트 로고

---
+++ BaekJoon/Solved/5338.py
@@ -0,0 +1,11 @@
+# BOJ 5338 : 마이크로소프트 로고
+# https://www.acmicpc.net/problem/5338
+
+
+import sys ; input = sys.stdin.readline
+
+print('''       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|''')
