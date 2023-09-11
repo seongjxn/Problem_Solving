622e6e37c9bbdba908dbe72196dc30716861a0c6
seongjxn <seongjxn@ivyforest.kr>
2023-09-11 23:04:35 +0900

    5339 - 콜센터

---
+++ BaekJoon/Solved/5339.py
@@ -0,0 +1,17 @@
+# BOJ 5339 : 콜센터
+# https://www.acmicpc.net/problem/5339
+
+
+import sys ; input = sys.stdin.readline
+
+
+print('''     /~\\
+    ( oo|
+    _\=/_
+   /  _  \\
+  //|/.\|\\\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |''')
